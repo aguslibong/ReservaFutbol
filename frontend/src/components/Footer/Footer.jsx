77e8c0dc63d10cb36f34574fbe629d74df989f4c
Loyola <ignaciotloyola@gmail.com>
2024-06-27 13:49:02 -0300

    Inicio modificado

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
-import futbolLoyaImage from '../../assets/FutbolLoya.jpeg';
 import './Footer.css';
 
 const Footer = () => {
@@ -11,7 +10,7 @@ const Footer = () => {
         <Row className="justify-content-start">
           <Col md={4} className="footer-map">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3274.466745539213!2d-57.96149722404073!3d-34.92146937304942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e8a42a403b2d%3A0x5d42cbbf3e1c2822!2sGimnasia%20y%20Esgrima%20La%20Plata!5e0!3m2!1sen!2sar!4v1622572035000!5m2!1sen!2sar"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3274.466745539213!2d-57.96149722404073!3d-34.92146937304942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e8a42a403b2d%3A0x5d42cbbf3e1c2822!2sCalle%20av.%20Col%C3%B3n%2012%2C%20C%C3%B3rdoba%2C%20Argentina!5e0!3m2!1ses!2sar!4v1687979841000!5m2!1ses!2sar"
               width="100%"
               height="200"
               style={{ border: 0 }}
@@ -24,7 +23,7 @@ const Footer = () => {
             <div className="contact-info">
               <p>Teléfono: +1 (234) 567-8901</p>
               <p>Email: canchasLoya@gmail.com</p>
-              <p>Dirección: Calle av. colón 12, Córdoba, Argentina</p>
+              <p>Dirección: Calle av. Colón 12, Córdoba, Argentina</p>
             </div>
           </Col>
           <Col md={4} className="footer-social">
@@ -43,7 +42,6 @@ const Footer = () => {
           <Col className="text-center mt-3">
             <p>&copy; 2024 - 2025 Canchas Loya. Todos los derechos reservados.</p>
           </Col>
-          
         </Row>
       </Container>
     </footer>
--- frontend/src/components/Inicio.jsx
+++ frontend/src/components/Inicio.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 import futbolLoyaImage from '../assets/FutbolLoya.jpeg';
 
 export default function Inicio() {
-    return (
-        <>
-            <div style={{ textAlign: 'center', padding: '20px' }}>
-                <h1>Bienvenidos a Canchas Loya</h1>
-                <p>
-                    En Canchas Loya, nos apasiona el fútbol. Ofrecemos las mejores instalaciones para que disfrutes del deporte rey con tus amigos y familiares. Nuestras canchas están equipadas con la mejor tecnología y mantenimiento, asegurando una experiencia de juego de alta calidad.
-                </p>
-                <img src={futbolLoyaImage} alt="Canchas de Fútbol Loya" style={{ width: '30%', height: 'auto', marginTop: '20px' }} />
-            </div>
-        </>
-    );
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+      <Card style={{ width: '30rem', textAlign: 'center' }}>
+        <Card.Img variant="top" src={futbolLoyaImage} alt="Canchas de Fútbol Loya" />
+        <Card.Body>
+          <Card.Title>Bienvenidos a Canchas Loya</Card.Title>
+          <Card.Text>
+            En Canchas Loya, nos apasiona el fútbol. Ofrecemos las mejores instalaciones para que disfrutes del deporte rey con tus amigos y familiares. Nuestras canchas están equipadas con la mejor tecnología y mantenimiento, asegurando una experiencia de juego de alta calidad.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
+
+
